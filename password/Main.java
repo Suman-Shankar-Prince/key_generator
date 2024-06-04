839644553d6a7c426e1d27b858d2698baaf41ad6
real@Suman <sumanshankarprince@hotmail.com>
2024-06-04 20:16:57 +0530

    Main file
    Last in the queue.

---
+++ password/Main.java
@@ -0,0 +1,16 @@
+package password;
+
+import password.MasterKey.Wallet;
+
+public class Main extends Wallet implements Runnable{
+	
+	public Main() {
+		super();
+	}
+	
+	@Override
+	public void run() {
+		
+		
+	}
+}
